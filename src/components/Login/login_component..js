00fd287c63220782d77ab9095b94be69150a3c3e
Bruno <brunonci@hotmail.com>
2023-08-01 14:17:10 -0300

    primer commit

---
+++ src/components/Login/login_component..js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { userLogin } from 'react-admin';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+
+class MyLoginPage extends Component {
+    submit = (e) => {
+        e.preventDefault();
+        // gather your data/credentials here
+        const credentials = { };
+
+        // Dispatch the userLogin action (injected by connect)
+        this.props.userLogin(credentials);
+    }
+
+    render() {
+        return (
+            <MuiThemeProvider theme={this.props.theme}>
+                <form onSubmit={this.submit}>
+                ...
+                </form>
+            </MuiThemeProvider>
+        );
+    }
+};
+
+export default connect(undefined, { userLogin })(MyLoginPage);
+
+// in src/MyLogoutButton.js
+import React from 'react';
+import { connect } from 'react-redux';
+import { Responsive, userLogout } from 'react-admin';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import ExitIcon from '@material-ui/icons/PowerSettingsNew';
+
+const MyLogoutButton = ({ userLogout, ...rest }) => (
+    <Responsive
+        xsmall={
+            <MenuItem
+                onClick={userLogout}
+                {...rest}
+            >
+                <ExitIcon /> Logout
+            </MenuItem>
+        }
+        medium={
+            <Button
+                onClick={userLogout}
+                size="small"
+                {...rest}
+            >
+                <ExitIcon /> Logout
+            </Button>
+        }
+    />
+);
+export default connect(undefined, { userLogout })(MyLogoutButton);
+
+// in src/App.js
+import MyLoginPage from './MyLoginPage';
+import MyLogoutButton from './MyLogoutButton';
+
+const App = () => (
+    <Admin loginPage={MyLoginPage} logoutButton={MyLogoutButton} authProvider={authProvider}>
+    ...
+    </Admin>
+);
